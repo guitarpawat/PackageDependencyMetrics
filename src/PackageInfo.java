29f021a3f787078f9e874aea281fea1a3b72eed9
wuttipat.ni@ku.th <wuttipat.ni@ku.th>
2018-11-09 15:12:41 +0900

    class commit

--- src/PackageInfo.java
+++ src/PackageInfo.java
@@ -1,15 +1,24 @@
 public class PackageInfo {
 
     String packageName;
+    
     int ca;
     int ce;
+    
+    int na;
+    int nc;
 
+
+    public PackageInfo(String packageName, int ca,int ce,int na, int nc){
+    	this.ca = ca;
+    	this.ce = ce;
+    	this.na = na;
+    	this.nc = nc;
+    }
+    
     public double getInstability(){
         return ce/(ca+ce);
     }
-
-    int na;
-    int nc;
 
     public double getAbstactness(){
         return na/nc;
