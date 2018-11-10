a19079d31f6d361bd09a2730a1a2c9e6d308650a
wuttipat.ni@ku.th <wuttipat.ni@ku.th>
2018-11-10 15:14:38 +0900

    get IA

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import stat.PackageStat;
 import stat.PackageUsage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     static Map<String, Integer> usage;
+    
+    static List<PackageInfo> listOfInfo = new ArrayList<PackageInfo>();
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -14,7 +18,13 @@ public class Main {
         PackageStat stat = new PackageStat(path);
         PackageUsage u = new PackageUsage(stat);
         usage = u.getPackages();
-        print(stat);
+        walkFrom(stat);
+        for(PackageInfo x : listOfInfo) {
+        	System.out.println("name: "+x.packageName);
+        	System.out.println("A: "+x.getAbstactness());
+        	System.out.println("I: "+x.getInstability());
+        	System.out.println("D': "+x.getDdass());
+        }
     }
 
     public static void print(PackageStat root) {
@@ -33,5 +43,21 @@ public class Main {
         System.out.println("No. of imports: "+ps.getImports());
         System.out.println("No. of class uses: "+usage.getOrDefault(ps.getPackageName(), 0));
         System.out.println("===========================================");
+    }
+    
+    
+    public static void walkFrom(PackageStat root) {
+        if(!root.getPackageName().isEmpty()) {
+        	PackageInfo x = createpckgInfo(root);
+//        	System.out.println(x.toString());
+        	listOfInfo.add(x);
+        }
+        for(PackageStat ps : root.getSubPackages()) {
+        	walkFrom(ps);
+        }
+    }
+
+    public static PackageInfo createpckgInfo(PackageStat ps) {
+    	return new PackageInfo(ps.getPackageName(), usage.getOrDefault(ps.getPackageName(), 0) , ps.getImports(), ps.getAbstracts() , ps.getClasses().size());
     }
 }
--- src/PackageInfo.java
+++ src/PackageInfo.java
@@ -1,18 +1,36 @@
 public class PackageInfo {
 
     String packageName;
-    int ca;
-    int ce;
+    
+    double ca;
+    double ce;
+    
+    double na;
+    double nc;
 
+
+    public PackageInfo(String packageName, double ca,double ce,double na, double nc){
+    	this.ca = ca;
+    	this.ce = ce;
+    	this.na = na;
+    	this.nc = nc;
+    	this.packageName = packageName;
+    }
+    
     public double getInstability(){
         return ce/(ca+ce);
     }
 
-    int na;
-    int nc;
-
     public double getAbstactness(){
         return na/nc;
+    }
+    
+    public String toString() {
+    	return packageName;
+    }
+    
+    public double getDdass() {
+    	return Math.abs(getInstability() + getAbstactness() - 1);
     }
 
 }
